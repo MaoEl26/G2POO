ff12612d4fd80a785be667174e44f91deb9c13b7
Mauricio Castillo <m.c.v.2695@gmail.com>
2017-03-31 03:03:10 +0000

    Replace Cell.java

--- StrategyGame/src/strategygame/Cell.java
+++ StrategyGame/src/strategygame/Cell.java
@@ -1,13 +1,53 @@
-
 package strategygame;
-/**
- *
- * @author Mauricio Castillo
- */
+
 public class Cell {
     private boolean passable;
     private Unit army;
     
-    //Metodo de impresion de la celda, si es franqueable y
-    //y si tiene una tropa 
+    // Método de impresión de la celda, si es franqueable y
+    // y si tiene una tropa.
+    
+    public Cell()
+    {
+        this.passable = false;
+        this.army = null;       // Puesto que todavía no hay nadie.       
+    }
+    
+    public boolean getPassable()
+    {
+        return this.passable;
+    }
+    
+    // Para poder cambiar el valor.
+    
+    public void setPassable(boolean value)   
+    {
+        this.passable = value;
+    }
+    
+    public char getArmy()
+    {
+        return this.army.getType();
+    }
+    
+    // Información de la celda, franqueabilidad y estado de la unidad.
+    
+    public void setArmy(Unit army)
+    {
+        this.army = army;
+    }
+    
+    public String toString()
+    {
+        String msj = "";
+        msj = msj + "Franqueable: " + getPassable() + "\n";
+        msj = msj + "Unidad: " + getArmy() + "\n";
+        
+        if (this.passable == true)
+        {
+            msj = msj + "Tipo: " + this.army.getType() + "\n";
+        }
+        
+        return msj;
+    }
 }
