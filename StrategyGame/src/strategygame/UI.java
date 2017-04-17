32d9fa37587f59aff59cfa7a0bb20d795b6e6b6e
Juan Diego Escobar <juan066@hotmail.es>
2017-04-17 03:02:58 +0000

    Update UI.java

--- StrategyGame/src/strategygame/UI.java
+++ StrategyGame/src/strategygame/UI.java
@@ -76,7 +76,7 @@ public final class UI implements Serializable{
             }
             
             player1.play();
-            System.out.println(player1.getName()+"\nDo you "
+            System.out.println(player2.getName()+"\nDo you "
                     + "want to save the current progress? (Y/N)");
             answer = scanner.next().toUpperCase();
             
@@ -150,8 +150,7 @@ public final class UI implements Serializable{
         System.out.println("Enter the stored game number you want to load (1 - 2 - 3)");
         cargado = stScanner.nextInt();
         
-        if ((1>=cargado)&&(cargado<3)){
-            System.out.println("in");
+        if ((cargado>=1)&&(cargado<=3)){
             File miFile = new File("StrategyGame "+cargado);
             System.out.println(miFile.getName());
             try{  
